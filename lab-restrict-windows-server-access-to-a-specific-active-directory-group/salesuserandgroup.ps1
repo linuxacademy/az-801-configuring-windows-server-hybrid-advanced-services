945034d04aeaebfa2738d2391a60c8084647c73e
Alex Potasnick <alexpotasnick@gmail.com>
2022-04-04 10:58:38 -0400

    update files

--- lab-restrict-windows-server-access-to-a-specific-active-directory-group/salesuserandgroup.ps1
+++ lab-restrict-windows-server-access-to-a-specific-active-directory-group/salesuserandgroup.ps1
@@ -1,10 +1,6 @@
 # Set user password
 $pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
 
-#Create domain admin
-New-ADUser -Name "awesomeadmin" -Description "lab domain admin" -Enabled $true -AccountPassword $pw
-Add-ADGroupMember -Identity "Domain Admins" -Members awesomeadmin
-
 #Create "Sales" group and add sales user
 New-ADGroup -Name "Sales" -SamAccountName Sales `
     -GroupCategory Security  `
