c63009fda6dbd34efdfc14a4b7cdc6c54f9d47ae
alexanderpotasnick <72271347+alexanderpotasnick@users.noreply.github.com>
2022-04-04 14:23:07 -0400

    Delete domainjoin.ps1

--- lab-restrict-windows-server-access-to-a-specific-active-directory-group/domainjoin.ps1
+++
@@ -1,9 +0,0 @@
-# Join computer corp.awesome.com domain
-
-$pw = 'p@55w0rd'
-
-$joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
-    UserName = "CORP\awesomeadmin"
-    Password = (ConvertTo-SecureString -String $pw -AsPlainText -Force)[0]
-})
-Add-Computer -Domain "corp.awesome.com" -Credential $joinCred
