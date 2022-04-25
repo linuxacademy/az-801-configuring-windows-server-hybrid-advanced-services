0b2c47819a62d966b3fdf51d7929270f629a4d43
Alex Potasnick <alexpotasnick@gmail.com>
2022-04-25 10:34:55 -0400

    add files

---
+++ lab-configuring-hyper-v-replicas/createvm.ps1
@@ -0,0 +1,14 @@
+ #Create Virtual Switch
+ New-VMSwitch -Name "External VM Switch" -AllowManagementOS $True -NetAdapterName "Ethernet"
+
+# Set VM Name, Switch Name, and Installation Media Path.
+$VMName =  "hyperv$env:computername" 
+$Switch = (Get-VMSwitch).Name
+
+# Create New Virtual Machine
+New-VM -Name $VMName -SwitchName $Switch `
+    -MemoryStartupBytes 2GB `
+    -Generation 2 `
+    -NewVHDPath "C:\Virtual Machines\$VMName\$VMName.vhdx" `
+    -NewVHDSizeBytes 20GB `
+    -Path "C:\Virtual Machines\$VMName"
