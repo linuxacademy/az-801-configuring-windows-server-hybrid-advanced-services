9f355c5bbc04e612b24ad532b8fbe5f42d500b43
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2022-07-14 17:49:49 -0500

    Update Powershell Custom Script Extension

--- 02 - Secure Windows Server Operating System/L02 - Configuring and Managing Microsoft Defender Application Control/installadds.ps1
+++ 02 - Secure Windows Server Operating System/L02 - Configuring and Managing Microsoft Defender Application Control/installadds.ps1
@@ -1,7 +1,12 @@
 # Install AD DS and configure forest
 
+param
+(
+      [string]$password,
+)
+
 $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
-$pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
+$pw = ConvertTo-SecureString "$password" -AsPlainText -Force
 Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force -NoRebootOnCompletion
