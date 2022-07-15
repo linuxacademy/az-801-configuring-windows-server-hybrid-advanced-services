e5c82212160dd6ea00717189611c046ee2c83782
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2022-07-15 10:37:39 -0500

    Add user and group creation to powershell

--- 02 - Secure Windows Server Operating System/create-domainObjects.ps1
+++ 02 - Secure Windows Server Operating System/create-domainObjects.ps1
@@ -6,6 +6,15 @@ New-ADOrganizationalUnit `
 -Name "Admins" `
 -Path "DC=coolcloudgurus,DC=COM"
 New-ADOrganizationalUnit `
+-Name "Tier 0 Admins" `
+-Path "OU=Admins,DC=coolcloudgurus,DC=COM"
+New-ADOrganizationalUnit `
+-Name "Tier 1 Admins" `
+-Path "OU=Admins,DC=coolcloudgurus,DC=COM"
+New-ADOrganizationalUnit `
+-Name "Tier 2 Admins" `
+-Path "OU=Admins,DC=coolcloudgurus,DC=COM"
+New-ADOrganizationalUnit `
 -Name "Servers" `
 -Path "OU=Product Engineering,DC=coolcloudgurus,DC=COM"
 New-ADOrganizationalUnit `
@@ -22,7 +31,7 @@ New-ADGroup `
 -GroupCategory Security `
 -GroupScope Global `
 -DisplayName "Admins Tier 0" `
--Path "CN=Users,DC=coolcloudgurus,DC=Com" `
+-Path "OU=Tier 0 Admins,OU=Admins,DC=coolcloudgurus,DC=Com" `
 -Description "Tier 0 Admins"
 New-ADGroup `
 -Name "Admins Tier 1" `
@@ -30,7 +39,7 @@ New-ADGroup `
 -GroupCategory Security `
 -GroupScope Global `
 -DisplayName "Admins Tier 1" `
--Path "CN=Users,DC=coolcloudgurus,DC=Com" `
+-Path "OU=Tier 1 Admins,OU=Admins,DC=coolcloudgurus,DC=Com" `
 -Description "Tier 1 Admins"
 New-ADGroup `
 -Name "Admins Tier 2" `
@@ -38,13 +47,89 @@ New-ADGroup `
 -GroupCategory Security `
 -GroupScope Global `
 -DisplayName "Admins Tier 2" `
--Path "CN=Users,DC=coolcloudgurus,DC=Com" `
+-Path "OU=Tier 2 Admins,OU=Admins,DC=coolcloudgurus,DC=Com" `
 -Description "Tier 2 Admins"
 
 # Create Admin Users in Admin Groups
 New-ADUser `
 -Name "Tier0Admin" `
 -OtherAttributes @{'title'="Tier0Admin";'mail'="tier0admind@coolcloudgurus.com"}
+New-ADUser `
+-Name "Tier1Admin" `
+-OtherAttributes @{'title'="Tier1Admin";'mail'="tier1admind@coolcloudgurus.com"}
+New-ADUser `
+-Name "Tier2Admin" `
+-OtherAttributes @{'title'="Tier2Admin";'mail'="tier2admind@coolcloudgurus.com"}
+
 Add-ADGroupMember `
--Identity "AdminTier0" `
+-Identity "AdminsTier0" `
 -Members Tier0Admin
+Add-ADGroupMember `
+-Identity "AdminsTier1" `
+-Members Tier1Admin
+Add-ADGroupMember `
+-Identity "AdminsTier2" `
+-Members Tier2Admin
+
+# Grant access to groups to OUs
+$OrganizationalUnit = "OU=Domain Controllers,DC=coolcloudgurus,DC=COM"
+$GroupName = "AdminsTier0"
+    
+Set-Location AD:
+$Group = Get-ADGroup -Identity $GroupName
+$GroupSID = [System.Security.Principal.SecurityIdentifier] $Group.SID
+$ACL = Get-Acl -Path $OrganizationalUnit
+    
+$Identity = [System.Security.Principal.IdentityReference] $GroupSID
+$ADRight = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
+$Type = [System.Security.AccessControl.AccessControlType] "Allow"
+$InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "None"
+$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($Identity, $ADRight, $Type,  $InheritanceType)
+    
+$ACL.AddAccessRule($Rule)
+Set-Acl -Path $OrganizationalUnit -AclObject $ACL
+
+
+$OrganizationalUnit = "OU=Servers,OU=Product Engineering,DC=coolcloudgurus,DC=COM"
+$GroupName = "AdminsTier1"
+    
+Set-Location AD:
+$Group = Get-ADGroup -Identity $GroupName
+$GroupSID = [System.Security.Principal.SecurityIdentifier] $Group.SID
+$ACL = Get-Acl -Path $OrganizationalUnit
+    
+$Identity = [System.Security.Principal.IdentityReference] $GroupSID
+$ADRight = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
+$Type = [System.Security.AccessControl.AccessControlType] "Allow"
+$InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "None"
+$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($Identity, $ADRight, $Type,  $InheritanceType)
+    
+$ACL.AddAccessRule($Rule)
+Set-Acl -Path $OrganizationalUnit -AclObject $ACL
+
+$OrganizationalUnit = "OU=Workstations,OU=Product Engineering,DC=coolcloudgurus,DC=COM"
+$GroupName = "AdminsTier2"
+    
+Set-Location AD:
+$Group = Get-ADGroup -Identity $GroupName
+$GroupSID = [System.Security.Principal.SecurityIdentifier] $Group.SID
+$ACL = Get-Acl -Path $OrganizationalUnit
+    
+$Identity = [System.Security.Principal.IdentityReference] $GroupSID
+$ADRight = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
+$Type = [System.Security.AccessControl.AccessControlType] "Allow"
+$InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "None"
+$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($Identity, $ADRight, $Type,  $InheritanceType)
+    
+$ACL.AddAccessRule($Rule)
+Set-Acl -Path $OrganizationalUnit -AclObject $ACL
+
+# Create Tier 0 GPO
+New-GPO -Name "Tier 0 GPO"
+New-GPLink -Name "Tier 0 GPO" -Target "OU=Domain Controllers,DC=coolcloudgurus,DC=COM"
+New-GPO -Name "Tier 1 GPO"
+New-GPLink -Name "Tier 1 GPO" -Target "OU=Servers,OU=Product Engineering,DC=coolcloudgurus,DC=COM"
+New-GPO -Name "Tier 2 GPO"
+New-GPLink -Name "Tier 2 GPO" -Target "OU=Workstations,OU=Product Engineering,DC=coolcloudgurus,DC=COM"
+
+
