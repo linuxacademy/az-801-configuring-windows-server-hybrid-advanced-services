b85baa7310b0c67cb3f0e6ececc4da9cd5de916b
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2022-07-14 19:00:27 -0500

    Updating double quotes in powershell script around securestring

--- 02 - Secure Windows Server Operating System/L02 - Configuring and Managing Microsoft Defender Application Control/installadds.ps1
+++ 02 - Secure Windows Server Operating System/L02 - Configuring and Managing Microsoft Defender Application Control/installadds.ps1
@@ -9,5 +9,5 @@ $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
 #$pw = ConvertTo-SecureString "$addsPassword" -AsPlainText -Force
-$addsPassword = ConvertTo-SecureString "$addsPassword" -AsPlainText -Force
+$addsPassword = ConvertTo-SecureString $addsPassword -AsPlainText -Force
 Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $addsPassword -DomainNetBIOSName 'CORP' -InstallDns -Force -NoRebootOnCompletion
