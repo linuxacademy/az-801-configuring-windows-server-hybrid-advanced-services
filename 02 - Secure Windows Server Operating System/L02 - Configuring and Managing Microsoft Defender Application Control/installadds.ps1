4df0ccc2ea7b8600ae49e2dbed646eae07ae7ed4
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2022-07-14 19:21:01 -0500

    Update powershell script to fix issue with passing secure string to non-secure string and unable to convert back to secure string

--- 02 - Secure Windows Server Operating System/L02 - Configuring and Managing Microsoft Defender Application Control/installadds.ps1
+++ 02 - Secure Windows Server Operating System/L02 - Configuring and Managing Microsoft Defender Application Control/installadds.ps1
@@ -8,6 +8,18 @@ param
 $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
-#$pw = ConvertTo-SecureString "$addsPassword" -AsPlainText -Force
-$addsPassword = ConvertTo-SecureString $addsPassword -AsPlainText -Force
-Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $addsPassword -DomainNetBIOSName 'CORP' -InstallDns -Force -NoRebootOnCompletion
+$pw = ConvertTo-SecureString "$addsPassword" -AsPlainText -Force
+#Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force -NoRebootOnCompletion
+Install-ADDSForest `
+-SafeModeAdministratorPassword $pw
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "coolcloudgurus.com" `
+-DomainNetbiosName "COOLCLOUDGURUS" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$true `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
