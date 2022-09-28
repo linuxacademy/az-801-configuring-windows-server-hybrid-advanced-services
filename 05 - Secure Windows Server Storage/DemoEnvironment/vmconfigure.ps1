58ba567872afe11aae6da76aa258fc31f4ad61f0
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2022-09-28 06:44:02 -0700

    Added automation for setup

--- 05 - Secure Windows Server Storage/DemoEnvironment/vmconfigure.ps1
+++ 05 - Secure Windows Server Storage/DemoEnvironment/vmconfigure.ps1
@@ -1,6 +1,16 @@
+$DomainName = "corp.awesome.com"
+$VMName = "dc"
+$User = "CORP\awesomeadmin"
+$PWord = ConvertTo-SecureString -String "p@55w0rd" -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+
 $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
+
+start-sleep -s 180 # Waiting for the DC to be configured
+
+Add-Computer -DomainName $DomainName -Credential $credential -Restart -Force 
