d17b475555f79953e561fd35890b1de5f2703a66
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2022-09-28 06:41:27 -0700

    Added automation for setup

--- 05 - Secure Windows Server Storage/DemoEnvironment/dcconfigure.ps1
+++ 05 - Secure Windows Server Storage/DemoEnvironment/dcconfigure.ps1
@@ -6,4 +6,13 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
 Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
 $pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
+
+mkdir "C:\PS"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/05%20-%20Secure%20Windows%20Server%20Storage/DemoEnvironment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
+
+$Trigger= New-ScheduledTaskTrigger -AtStartup # Specify the trigger settings
+$User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\PS\adusersetup.ps1" # Specify what program to run and with its parameters
+Register-ScheduledTask -TaskName "create ad account" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force # Specify the name of the task
+
 Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force
