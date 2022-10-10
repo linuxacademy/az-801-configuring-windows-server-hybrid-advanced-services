8ff6ca0144cd50b1c39016b185627e3449d97e3c
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2022-10-10 10:46:22 -0700

    Update paths

--- 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/dcconfigure.ps1
+++ 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/dcconfigure.ps1
@@ -8,7 +8,7 @@ Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
 $pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
 
 mkdir "C:\PS"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/05%20-%20Secure%20Windows%20Server%20Storage/DemoEnvironment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/06%20-%20Implement%20a%20Windows%20Server%20Failover%20Cluster/DemoEnvironment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
 
 $Trigger= New-ScheduledTaskTrigger -AtStartup # Specify the trigger settings
 $User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
