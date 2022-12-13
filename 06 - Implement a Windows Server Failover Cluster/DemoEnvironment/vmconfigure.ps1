a124c1205e853a8ebed49bccbafb8a7781edc91d
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2022-12-13 06:28:03 -0800

    Update vmconfigure.ps1

--- 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/vmconfigure.ps1
+++ 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/vmconfigure.ps1
@@ -11,6 +11,25 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
 
-start-sleep -s 240 # Waiting for the DC to be configured
+New-NetFirewallRule -DisplayName "AzureILBProbe" -Direction Inbound -LocalPort 59999 -Protocol TCP -Action Allow
 
-Add-Computer -DomainName $DomainName -Credential $credential -Restart -Force 
+Enable-PSRemoting -Force
+winrm set winrm/config/service/auth '@{Kerberos="true"}'
+
+
+Do {
+
+    Try {    
+        $Error.Clear() 
+        $join = Add-Computer -DomainName $DomainName -Credential $credential -Force
+        Start-Sleep -s 5 
+    }
+
+    Catch {
+        $Error[0].Exception
+    }
+
+} While ( $Error.Count -eq 1 )
+
+shutdown.exe /r /t 20
+exit
