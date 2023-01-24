998691932aab49551f5a8ded838521517a3b851d
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-01-24 10:17:17 -0600

    add demo env deployment

---
+++ 02 - Secure Windows Server Operating System/DemoEnvironment/adusersetup.ps1
@@ -0,0 +1,19 @@
+start-sleep -s 30 # Giving the DC time to start
+
+$username = "awesomeadmin"
+$pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
+
+#Create domain admin
+New-ADUser -Name "awesomeadmin" -Description "lab domain admin" -Enabled $true -AccountPassword $pw
+Add-ADGroupMember -Identity "Domain Admins" -Members awesomeadmin
+
+#Create "Sales" group and add sales user
+New-ADGroup -Name "Sales" -SamAccountName Sales `
+    -GroupCategory Security  `
+    -GroupScope Global `
+    -DisplayName "Sales Team" `
+    -Path "CN=Users,DC=corp,DC=awesome,DC=com" `
+    -Description "Members of the Sales Team"
+
+New-ADUser -Name "awesomesales" -Description "sales team member" -Enabled $true -AccountPassword $pw
+Add-ADGroupMember -Identity "Sales" -Members awesomesales
