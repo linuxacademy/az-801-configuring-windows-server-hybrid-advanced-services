8c3d9593f808aa2a8decfaeb9ea5693f24859ffa
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-01-24 13:57:52 -0600

    Move disable IEESC and WDAC install to different script

--- 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/dcconfigure.ps1
+++ 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/dcconfigure.ps1
@@ -20,26 +20,4 @@ $Task.Triggers.Repetition.Interval = 'PT1M'
 $Task.Triggers.Repetition.Duration = 'PT10M'
 $Task | Set-ScheduledTask
 
-Invoke-WebRequest -Uri "https://webapp-wdac-wizard.azurewebsites.net/packages/WDACWizard_2.1.0.1_x64_8wekyb3d8bbwe.MSIX" -OutFile "$env:USERPROFILE/Downloads/WDACWizard_2.1.0.1_x64_8wekyb3d8bbwe.MSIX"
-
-function Disable-IEESC {
-
-$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-
-$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-
-Stop-Process -Name Explorer
-
-Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
-
-}
-
-Disable-IEESC
-
-Add-AppxPackage -Path "C:\Users\azureuser\Downloads\WDACWizard_2.1.0.1_x64_8wekyb3d8bbwe.MSIX"
-
 Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force
