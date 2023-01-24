e5301987220be783068f9ea7c19d3ef9c4374682
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-01-24 12:07:55 -0600

    Add function to dcconfigure.ps1 to disable IE ESC

--- 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/dcconfigure.ps1
+++ 06 - Implement a Windows Server Failover Cluster/DemoEnvironment/dcconfigure.ps1
@@ -20,4 +20,22 @@ $Task.Triggers.Repetition.Interval = 'PT1M'
 $Task.Triggers.Repetition.Duration = 'PT10M'
 $Task | Set-ScheduledTask
 
+function Disable-IEESC {
+
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+Stop-Process -Name Explorer
+
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+}
+
+Disable-IEESC
+
 Install-ADDSForest -DomainName "corp.awesome.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force
