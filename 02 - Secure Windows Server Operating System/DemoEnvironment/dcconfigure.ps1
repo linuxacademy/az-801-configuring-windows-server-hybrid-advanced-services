4c05741e1d337b4a05c336ec250cad159ae082db
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-01-24 10:22:09 -0600

    Update github raw link url in demoEnv

--- 02 - Secure Windows Server Operating System/DemoEnvironment/dcconfigure.ps1
+++ 02 - Secure Windows Server Operating System/DemoEnvironment/dcconfigure.ps1
@@ -8,7 +8,7 @@ Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
 $pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
 
 mkdir "C:\PS"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/06%20-%20Implement%20a%20Windows%20Server%20Failover%20Cluster/DemoEnvironment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/02%20-%20Secure%20Windows%20Server%20Operating%20System/DemoEnvironment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
 
 $Trigger= New-ScheduledTaskTrigger -AtStartup
 $User= "NT AUTHORITY\SYSTEM"
