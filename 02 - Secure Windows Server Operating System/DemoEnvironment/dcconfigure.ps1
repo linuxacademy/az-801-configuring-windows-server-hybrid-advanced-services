dab70f0217e29ea9e980c5f7b64a6915d3904eb3
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-01-24 22:32:36 -0600

    Replace single quote with double

--- 02 - Secure Windows Server Operating System/DemoEnvironment/dcconfigure.ps1
+++ 02 - Secure Windows Server Operating System/DemoEnvironment/dcconfigure.ps1
@@ -31,7 +31,7 @@ $username        = $Args[0]
 $localUser= "CORP\$username"
 # Set action to be executed at login of $localUser
 $userLoginAction= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-ExecutionPolicy Bypass -File C:\PS\installWDAC.ps1"
-Register-ScheduledTask -TaskName 'install wdac' -Trigger $userLoginTrigger -User $localUser -Action $userLoginAction -RunLevel Highest -Force
+Register-ScheduledTask -TaskName "install wdac" -Trigger $userLoginTrigger -User $localUser -Action $userLoginAction -RunLevel Highest -Force
 
 $userLoginTask = Get-ScheduledTask -TaskName 'install wdac'
 $userLoginTask.Triggers.Repetition.Interval = 'PT1M'
