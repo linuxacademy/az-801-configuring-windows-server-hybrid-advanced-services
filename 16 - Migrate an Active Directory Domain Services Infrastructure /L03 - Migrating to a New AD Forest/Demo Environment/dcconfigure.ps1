a5e585fec6f31eb6b0a004bc69f9fa93d79e1ec0
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2023-02-28 12:05:47 -0800

    Updated URL Paths

--- 16 - Migrate an Active Directory Domain Services Infrastructure /L03 - Migrating to a New AD Forest/Demo Environment/dcconfigure.ps1
+++ 16 - Migrate an Active Directory Domain Services Infrastructure /L03 - Migrating to a New AD Forest/Demo Environment/dcconfigure.ps1
@@ -8,7 +8,7 @@ Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
 $pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
 
 mkdir "C:\PS"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/clintbonnett-acg/az-801-testing/main/L03/Demo%20Environment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/16%20-%20Migrate%20an%20Active%20Directory%20Domain%20Services%20Infrastructure%20/L03%20-%20Migrating%20to%20a%20New%20AD%20Forest/Demo%20Environment/adusersetup.ps1" -OutFile "C:\PS\adusersetup.ps1"
 
 $Trigger= New-ScheduledTaskTrigger -AtStartup
 $User= "NT AUTHORITY\SYSTEM"
