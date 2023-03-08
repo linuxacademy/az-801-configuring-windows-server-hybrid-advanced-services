1cd1548877641d009a3461bbfc3adec1b5f94d7a
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-03-07 23:28:11 -0600

    Add disable of IEESC

--- 12 - Migrate On-Premises Storage to On-Premises Servers or Azure/vmconfigure.ps1
+++ 12 - Migrate On-Premises Storage to On-Premises Servers or Azure/vmconfigure.ps1
@@ -33,6 +33,24 @@ Register-ScheduledTask -TaskName "setup share" -Trigger $startUpTrigger -User $s
 $startUpTask = Get-ScheduledTask -TaskName 'setup share'
 $startUpTask | Set-ScheduledTask
 
+# Function to disable IEESC
+function Disable-IEESC {
+
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+Stop-Process -Name Explorer
+
+}
+
+# Call function
+Disable-IEESC
+
 Do {
 
     Try {    
