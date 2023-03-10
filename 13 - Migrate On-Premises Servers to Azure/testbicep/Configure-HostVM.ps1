b44f6b4b225e9bfa89feb7ea21ed3050673147e3
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-03-10 16:36:14 -0600

    Update logging entry logic

--- 13 - Migrate On-Premises Servers to Azure/testbicep/Configure-HostVM.ps1
+++ 13 - Migrate On-Premises Servers to Azure/testbicep/Configure-HostVM.ps1
@@ -33,6 +33,7 @@ Stop-Process -Name Explorer
 
 # Diable IEESC
 try{
+    Write-Log -Entry "Attempting to disable IEESC"
     Disable-IEESC
     Write-Log -Entry "Disabled IEESC successfully"
 }
@@ -42,25 +43,27 @@ catch {
 
 # Fix Server UI
 try {
+    Write-Log -Entry "Fix server UI - Processing..."
     Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
-    Write-Log -Entry "Fixed Server UI Successfully" 
+    Write-Log -Entry "Fixed server UI - Success" 
 }
 catch {
-    Write-Log -Entry "Fixed Server UI Failed"
+    Write-Log -Entry "Fixed server UI - Failure"
     Write-Log $_
 }
 
 #Download Scripts
 try {
+    Write-Log -Entry "Download HyperV VM creation script - Processing..."
     New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
     Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/linuxacademy/az-801-configuring-windows-server-hybrid-advanced-services/main/13%20-%20Migrate%20On-Premises%20Servers%20to%20Azure/testbicep/Create-VM.ps1' -OutFile 'C:\temp\Create-VM.ps1'
-    Write-Log -Entry "Download HyperV VM Creation Script Successfully"
+    Write-Log -Entry "Download HyperV VM creation script - Success"
 }
 catch {
-    Write-Log -Entry "Download HyperV VM Creation Script Failed"
+    Write-Log -Entry "Download HyperV VM creation script - Failure"
     Write-Log $_
 }
 
@@ -103,35 +106,68 @@ catch {
 # $VHDName = $VHDName.Replace(' ', '-')
 # $ParentVHDPath = "C:\Users\Public\Documents\$VHDName.vhd"
 
-# Create VMs
-try {
-    $VMs = @('nestedVM1')
-    foreach ($VM in $VMs) {
-        #Set Scheduled Tasks to create the VM after restart
-        $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VM.ps1 -UserName $($UserName) -Password $($Password) -VM $($VM)"
-        # Random dleay so both don't run at exactly the same time
-        $Trigger = New-ScheduledTaskTrigger -AtStartup
-        $Trigger.Delay = 'PT15S'
-        Register-ScheduledTask -TaskName "Create-VM $($VM)" -Action $Action -Trigger $Trigger -Description "Create VM" -RunLevel Highest -User "System"
-        Write-Log -Entry "Succeeded to Create Hyper VM Creation Scheduled Task for $VM"
-    }
-}
-catch {
-    Write-Log -Entry "Failed to Create HyperV VM Creation Scheduled Task for $VM"
-    Write-Log $_
-}
+# # VM Creation Loop
+# try {
+#     Write-Log -Entry "Creating loop - Processing..."
+#     $VMs = @('nestedVM1')
+#     foreach ($VM in $VMs) {
+#         try {
+#             Write-Log -Entry "Scheduled task creation for $VM - Processing..."
+#             # Create scheduled task action
+#             $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VM.ps1 -UserName $($UserName) -Password $($Password) -VM $($VM)"
+#             Write-Log -Entry "Scheduled task creation for $VM - Success"
+#         }
+#         catch {
+#             Write-Log -Entry "Scheduled task creation for $VM - Failure"
+#             Write-Log -Entry $_
+#         }
+#         try {
+#             # Create scheduled task trigger
+#             Write-Log -Entry "Schedule task trigger for $VM - Processing..."
+#             $Trigger = New-ScheduledTaskTrigger -AtStartup
+#             $Trigger.Delay = 'PT15S'
+#             Write-Log -Entry "Schedule task trigger for $VM - Success"
+#         }
+#         catch {
+#             Write-Log -Entry "Scheduled task creation for $VM - Failure"
+#             Write-Log -Entry $_
+#         }
+#         try {
+#             Write-Log -Entry "Scheduled task registration for $VM - Processing..."
+#             Register-ScheduledTask -TaskName "Create-VM $($VM)" -Action $Action -Trigger $Trigger -Description "Create VM" -RunLevel Highest -User "System"
+#             Write-Log -Entry "Scheduled task registration for $VM - Success"
+#         }
+#         catch {
+#             Write-Log -Entry "Scheduled task registration for $VM - Failure"
+#             Write-Log -Entry $_
+#         }
+#     }
+#     Write-Log -Entry "Creating loop - Success"
+# }
+# catch {
+#     Write-Log -Entry "Creating loop - Failure"
+#     Write-Log $_
+# }
 
 # Install Hyper-V
 try{
-    Write-Log -Entry "Attempting install of Hyper-V..."
+    Write-Log -Entry "Install Hyper-V - Processing..."
     Install-WindowsFeature -Name Hyper-V -IncludeAllSubFeature -IncludeManagementTools
     # Add-WindowsFeature Hyper-V -IncludeManagementTools
-    Write-Log -Entry "Succeeded in Install of HyperV"
+    Write-Log -Entry "Install Hyper-V - Success"
 }
 catch{
-    Write-Log -Entry "Failed to Install HyperV"
+    Write-Log -Entry "Install Hyper-V - Failure"
     Write-Log $_
 }
 
 #Restart the Server
-Restart-Computer -Force
+try {
+    Write-Log -Entry "Restart server - Processing..."
+    Restart-Computer -Force
+    Write-Log -Entry "Restart server - Success"   
+}
+catch {
+    Write-Log -Entry "Restart server - Failure"
+    Write-Log -Entry $_
+}
