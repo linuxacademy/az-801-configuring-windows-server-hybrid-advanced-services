3d3d5933d88b1aaebde94c9e4f1585cbfe3ccb38
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-03-15 12:11:07 -0500

    Move file share setup to invoke-command scriptblock

--- 13 - Migrate On-Premises Servers to Azure/testbicep/Create-VM.ps1
+++ 13 - Migrate On-Premises Servers to Azure/testbicep/Create-VM.ps1
@@ -284,29 +284,31 @@ try {
     # Setup File Share on Guest
     try {
         Write-Log -Entry "Configure share on Hyper-V Guest VM - Attempting..."
-        # Define variables
-        $shareName = "TestShare"
-        $directoryPath = "C:\$shareName"
-        $fullAccessUsers = "Everyone"
-        $readAccessUsers = "Everyone"
-        $numFiles = 3  # Change this to the number of files you want to create
-        $maxFileSize = 1MB  # Change this to the maximum file size you want to create
+        Invoke-Command -ScriptBlock {
+            # Define variables
+            $shareName = "TestShare"
+            $directoryPath = "C:\$shareName"
+            $fullAccessUsers = "Everyone"
+            $readAccessUsers = "Everyone"
+            $numFiles = 3  # Change this to the number of files you want to create
+            $maxFileSize = 1MB  # Change this to the maximum file size you want to create
 
-        # Create directory
-        New-Item -ItemType Directory -Path $directoryPath
+            # Create directory
+            New-Item -ItemType Directory -Path $directoryPath
 
-        # Set up SMB share
-        New-SmbShare -Name $shareName -Path $directoryPath -FullAccess $fullAccessUsers -ReadAccess $readAccessUsers
+            # Set up SMB share
+            New-SmbShare -Name $shareName -Path $directoryPath -FullAccess $fullAccessUsers -ReadAccess $readAccessUsers
 
-        # Create random files
-        $random = New-Object System.Random
-        for ($i = 1; $i -le $numFiles; $i++) {
-            $fileName = "File$i.txt"
-            $filePath = Join-Path $directoryPath $fileName
-            $fileSize = $random.Next(1, $maxFileSize)
-            $fileStream = New-Object IO.FileStream($filePath, [IO.FileMode]::Create)
-            $fileStream.SetLength($fileSize)
-            $fileStream.Close()
+            # Create random files
+            $random = New-Object System.Random
+            for ($i = 1; $i -le $numFiles; $i++) {
+                $fileName = "File$i.txt"
+                $filePath = Join-Path $directoryPath $fileName
+                $fileSize = $random.Next(1, $maxFileSize)
+                $fileStream = New-Object IO.FileStream($filePath, [IO.FileMode]::Create)
+                $fileStream.SetLength($fileSize)
+                $fileStream.Close()
+            } -VMName $VM -Credential $Credential
         }
         Write-Log -Entry "Configure share on Hyper-V Guest VM - Success"
     }
