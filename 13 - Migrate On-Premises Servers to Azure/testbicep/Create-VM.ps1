8cd63fe2bd3724a20b87166e3dfec7e2b73682f3
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-03-16 14:44:36 -0500

    Add disable IEESC via invoke-command to guest vm

--- 13 - Migrate On-Premises Servers to Azure/testbicep/Create-VM.ps1
+++ 13 - Migrate On-Premises Servers to Azure/testbicep/Create-VM.ps1
@@ -29,21 +29,6 @@ function Wait-VMPowerShellReady ($VM, $Credential) {
     }
 }
 
-# Function to disable IEESC
-function Disable-IEESC {
-
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-
-    Stop-Process -Name explorer
-
-}
-
 #Start a stopwatch to measure the deployment time
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
@@ -328,14 +313,24 @@ catch {
     Exit
 }
 
-$command = Disable-IEESC
+# Disable IEESC on guest vm
+$command = {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+    Stop-Process -Name explorer
+}
 try {
     Invoke-Command -ScriptBlock { $command } -VMName $VM -Credential $Credential
     Write-Log -Entry "Disable IEESC on $VM - Success"
 }
 catch {
     Write-Log -Entry "Disable IEESC on $VM - Failed"
-    Write-Log -Entry "$_"
 }
 
 #The end, stop stopwatch and display the time that it took to deploy
