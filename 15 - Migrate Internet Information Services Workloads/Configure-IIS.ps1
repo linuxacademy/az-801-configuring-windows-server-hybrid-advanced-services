b7390b49bceffb64abdeacec5a66d9f667a5229c
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2023-03-22 17:36:06 -0500

    Add dotnet 7.04 and 7.02

--- 15 - Migrate Internet Information Services Workloads/Configure-IIS.ps1
+++ 15 - Migrate Internet Information Services Workloads/Configure-IIS.ps1
@@ -19,6 +19,8 @@ try {
     Write-Log -Entry "Install dotnet webhost bundle - Processing..."
     Invoke-WebRequest -Uri 'https://download.visualstudio.microsoft.com/download/pr/c6ad374b-9b66-49ed-a140-588348d0c29a/78084d635f2a4011ccd65dc7fd9e83ce/dotnet-sdk-7.0.202-win-x64.exe' -OutFile 'C:\temp\dotnet-hosting.exe'
     Start-Process -FilePath "C:\temp\dotnet-hosting.exe" -ArgumentList @('/quiet', '/norestart') -Wait -PassThru
+    Invoke-WebRequest -Uri 'https://download.visualstudio.microsoft.com/download/pr/d97e0776-b316-4c41-a067-202eb027b968/e9694b0aa94e4b814f980f9ec3d3f400/dotnet-hosting-7.0.4-win.exe' -OutFile 'C:\temp\dotnet-hosting.exe'
+    Start-Process -FilePath "C:\temp\dotnet-hosting.exe" -ArgumentList @('/quiet', '/norestart') -Wait -PassThru
     Write-Log -Entry "Install dotnet webhost bundle - Success"
 }
 catch {
