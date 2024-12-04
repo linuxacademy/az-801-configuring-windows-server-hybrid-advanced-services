efe667f59ff4f7c3732ff7e9b58f82890d32631a
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2024-12-04 13:58:04 -0500

    Update UpgradeDomain.ps1

--- 16 - Migrate an Active Directory Domain Services Infrastructure /L04 - Upgrading an Existing Forest/UpgradeDomain.ps1
+++ 16 - Migrate an Active Directory Domain Services Infrastructure /L04 - Upgrading an Existing Forest/UpgradeDomain.ps1
@@ -2,7 +2,7 @@ Get-ADDomain | Select-Object DomainMode
 
 Move-ADDirectoryServerOperationMasterRole -Identity dc2 -OperationMasterRole SchemaMaster, DomainNamingMaster, PDCEmulator, RIDMaster, InfrastructureMaster -Confirm:$false
 
-Invoke-Command -ComputerName dc.corp.awesome.com -ScriptBlock {Uninstall-ADDSDomainController -DemoteOperationMasterRole -RemoveApplicationPartition}
+Invoke-Command -ComputerName dc.corp.awesome.com -ScriptBlock {Uninstall-ADDSDomainController -DemoteOperationMasterRole -RemoveApplicationPartition -IgnoreLastDNSServerForZone}
 
 Set-ADForestMode -Identity corp.awesome.com -ForestMode Windows2016Forest -Confirm:$false
 Set-ADDomainMode -identity corp.awesome.com -DomainMode Windows2016Domain -Confirm:$false
